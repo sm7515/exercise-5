8390c89c6e711d8437112426a0598c8b02892ff1
Sijia Meng <sm7515@nyu.edu>
2019-11-26 15:33:43 -0500

    add logout, signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 
 import Login from './pages/Login/index'
 import UserProfile from './pages/UserProfile/index'
+import Signup from './pages/Signup/index'
+import Logout from "./pages/Logout/index";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <UserProfile />
+      <Router>
+        <Route exact path='/' component={UserProfile} />
+        <Route exact path='/login' component={Login} />
+        <Route path='/sign-up' component={Signup} />
+        <Route path='/log-out' component={Logout} />
+      </Router>
     </div>
   );
 }
